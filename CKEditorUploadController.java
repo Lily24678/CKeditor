0c06e6398f73938913a36109f8640404b66d2d50
Lily24678 <lishanyun_lsy@163.com>
2019-08-23 13:46:39 +0800

    CKEdit4.5.0 文件上传

--- CKEditorUploadController.java
+++ CKEditorUploadController.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,15 +22,15 @@ import com.xiechanglei.code.utils.json.JsonUtil;
 /**
  * CKEditor4.5.0 文件处理
  * 
- * @author lishanyun 2019年8月14日2019年8月14日
+ * @author lishanyun 2019年8月14日
  */
 @Controller
 @RequestMapping("/")
-public class CKEditorUpload {
+public class CKEditorUploadController {
 
-	@RequestMapping("uploadImage")
+	@RequestMapping(value="uploadFile",method=RequestMethod.POST)
 	@ResponseBody
-	public void uploadImage(@RequestParam(value = "upload", required = false) MultipartFile file,
+	public void uploadFile(@RequestParam(value = "upload", required = false) MultipartFile file,
 			HttpServletRequest request, HttpServletResponse response) throws Exception {
 
 		//写文件
@@ -40,8 +41,8 @@ public class CKEditorUpload {
 		
 		Map<String, Object> result = new HashMap<String, Object>();
 		result.put("uploaded", fileSize);
-		result.put("fileName", "test.jpg");
-		result.put("url", "http://192.168.1.108:8020/CKeditor/image/test.jpg");
+		result.put("fileName", fileName);
+		result.put("url", "/CKeditor/image/"+fileName);
 
 		response.setCharacterEncoding("UTF-8");
 		PrintWriter out = response.getWriter();
